11ae99982119853f00fd6717583fad5379a5bdbf
Mahmoud <marwen.mahmoud2@dxc.com>
2024-07-05 23:51:44 +0100

    debugg page

--- pages/debug.js
+++ pages/debug.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// pages/debug.js
+import React, { useEffect } from 'react';
+import AWS from 'aws-sdk';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const DebugPage = () => {
+  useEffect(() => {
+    console.log('APP_ACCESS_KEY_ID:', process.env.APP_ACCESS_KEY_ID);
+    console.log('APP_ACCESS_KEY:', process.env.APP_ACCESS_KEY);
+    console.log('APP_REGION:', process.env.APP_REGION);
+
+    AWS.config.update({
+      accessKeyId: process.env.APP_ACCESS_KEY_ID,
+      secretAccessKey: process.env.APP_ACCESS_KEY,
+      region: process.env.APP_REGION,
+    });
+
+    console.log('AWS Config:', AWS.config);
+  }, []);
+
+  return <div>Check the console for environment variable logs.</div>;
+};
+
+export default DebugPage;
